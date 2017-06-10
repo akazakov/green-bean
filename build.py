68c62a67c0e6cf50b8dcb8f488cb30636ec3f07d
Artem Kazakov <kazakov@gmail.com>
2017-06-09 22:33:10 -0400

    add commands

--- build.py
+++ build.py
@@ -2,16 +2,41 @@
 import argparse
 
 
+def setup():
+    print("setup complete")
+
+
+def compile():
+    print("compile complete")
+
+
+def test():
+    print("test complete")
+
+
+def package():
+    print("package complete")
+
+
+steps = {
+    'setup': setup,
+    'compile': compile,
+    'test': test,
+    'package': package
+}
+
+
 def hello():
-    print("setup")
-    print("compile")
-    print("test")
-    print("package")
+    steps.keys()
 
 parser = argparse.ArgumentParser(description='Build it')
 parser.add_argument('--list_steps', action='store_true')
+parser.add_argument('--run', nargs=1)
 
 args = parser.parse_args()
 
 if args.list_steps:
     hello()
+
+if args.run:
+    steps[args.run]()
