d40db832866228e3d25814c73623709b9bbac5d7
Artem Kazakov <kazakov@gmail.com>
2017-06-09 22:20:50 -0400

    add commands

--- build.py
+++ build.py
@@ -3,7 +3,10 @@ import argparse
 
 
 def hello():
-    print("Hello")
+    print("setup")
+    print("compile")
+    print("test")
+    print("package")
 
 parser = argparse.ArgumentParser(description='Build it')
 parser.add_argument('--list_steps', action='store_true')
