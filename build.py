d0d0f92351d317a3ca9952d14c40633fdb1747db
Artem Kazakov <kazakov@gmail.com>
2017-06-09 22:04:42 -0400

    Start build

---
+++ build.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def hello():
+    print("Hello")
+
+parser = argparse.ArgumentParser(description='Build it')
+parser.add_argument('--list_steps', action='store_true')
+
+args = parser.parse_args()
+
+if args.list_steps:
+    hello()
